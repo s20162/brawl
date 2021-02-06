138057f929ba02b69b4a621d923c5ac11fa77d8a
adam_lichy <s20162@pjwstk.edu.pl>
2021-02-06 11:01:04 +0100

    deleted unused imports in GameServiceTest

--- src/test/java/lichysoft/brawl/service/GameServiceTest.java
+++ src/test/java/lichysoft/brawl/service/GameServiceTest.java
@@ -1,8 +1,6 @@
 package lichysoft.brawl.service;
 
 import lichysoft.brawl.model.Player;
-import org.junit.Rule;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
